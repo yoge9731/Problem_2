b6de81bea27cb2acfd6bdf1fbf4ad3dcb9dab6dc
YOGEESH K P <yogeesh793@gmail.com>
2025-01-30 13:23:11 +0530

    Add files via upload

---
+++ Problem_2.py
@@ -0,0 +1,17 @@
+"""String Manipulation: Write a Python Program that:
+* Take a sentence as input from the user.
+* Print the sentence all uppercase and lowercase.
+* Replace all spaces with underscore.
+* Removes leading and trailing whitespaces."""
+
+s=input("Enter sentence: ")
+print(s.upper( ))
+print(s.lower())
+print(s.replace(" ","_"))
+print(s.strip())
+
+y=input("enter a sentance: ")
+print(y.upper())
+print(y.lower())
+print(y.replace(" ","_"))
+print(y.strip())
